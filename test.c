30c77a046185cf23c18efa5a0d89145ae12ced9f
Seraf00 <53179542+Seraf00@users.noreply.github.com>
2024-11-28 17:18:48 +0100

    function addition

--- test.c
+++ test.c
@@ -4,7 +4,11 @@
 int main(){
     printf("hello , world");
     printf("hello , world from fares");
-    
+    //add function of addition
+    int a = 5;
+    int b = 6;
+    int c = a + b;
+    printf("the result of the addition is : %d",c);
 
 
     return 0;
